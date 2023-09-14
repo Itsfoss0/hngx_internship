1b73fdd557fdd71ce08a5fd45c81a57bbf2228b4
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-15 01:21:39 +0300

    [Chore]: Add more utils

--- 0x1-people/api/v1/utils/__init__.py
+++ 0x1-people/api/v1/utils/__init__.py
@@ -4,4 +4,11 @@
 package level imports for the utilities
 """
 
-from api.v1.utils.utils import user_exists, create_user
+from api.v1.utils.utils import (
+    user_exists,
+    create_user,
+    get_user_by_id,
+    get_user_by_name,
+    delete_user_id,
+    delete_user_name
+)
--- 0x1-people/api/v1/utils/utils.py
+++ 0x1-people/api/v1/utils/utils.py
@@ -5,7 +5,7 @@ Utility functions
 """
 from typing import Union
 from api.v1.db import cursor_object, db_object, DB_NAME
-from api.v1.exceptions import UserExistsAlready
+from api.v1.exceptions import UserExistsAlready, InvalidOperation, NoResultsFound
 
 def user_exists(name: str) -> bool:
     """
@@ -29,3 +29,49 @@ def create_user(user_name: str) -> dict:
         return {"user": f"{user_name}"}
 
     raise UserExistsAlready
+
+
+def get_user_by_id(user_id: int) -> dict:
+    """
+    Get user object
+    """
+    cursor_object.execute("SELECT id, name FROM people where id = %s", (user_id,))
+    results = cursor_object.fetchall()
+    if bool(results) is False:
+        raise NoResultsFound
+    return results[0]
+
+def get_user_by_name(user_name: str) -> dict:
+    """
+    Get user object
+    """
+    cursor_object.execute("SELECT id, name FROM people where name = %s", (user_name,))
+    results = cursor_object.fetchall()
+    if bool(results) is False:
+        raise NoResultsFound
+    return results[0]
+
+def delete_user_name(name: str) -> str:
+    """
+    Delete User object using the name
+    """
+    try:
+        if user_exists(name):
+            cursor_object.execute("DELETE FROM people where name = %s", (name, ))
+            db_object.commit()
+            return f"user {name} Deleted"
+        raise NoResultsFound
+    except Exception:
+        raise NoResultsFound
+
+def delete_user_id(id: int) -> str:
+    """
+    Delete user object using ID
+    """
+    try:
+        exists = get_user_by_id(id)
+        cursor_object.execute('DELETE FROM people where id = %s', (id, ))
+        db_object.commit()
+        return f"user id {id} Deleted"
+    except NoResultsFound:
+        raise NoResultsFound
