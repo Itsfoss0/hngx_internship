7851a4eedbd9e93d94ccedb2b4358a429cfcb064
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-15 01:22:02 +0300

    [Chore]: Handle Delete views

--- 0x1-people/api/v1/views/person.py
+++ 0x1-people/api/v1/views/person.py
@@ -9,8 +9,15 @@ for the API
 from api.v1.views import app_views
 from flask import make_response, request, abort, jsonify
 
-from api.v1.utils import create_user, user_exists
-from api.v1.exceptions import UserExistsAlready
+from api.v1.exceptions import UserExistsAlready, NoResultsFound, InvalidOperation
+from api.v1.utils import (
+    create_user,
+    get_user_by_id,
+    get_user_by_name,
+    delete_user_name,
+    delete_user_id
+)
+
 
 @app_views.route('', methods = ['GET','POST', 'DELETE', 'PUT'], strict_slashes=False)
 def handle_request():
@@ -22,8 +29,49 @@ def handle_request():
         user_name = details.get('name')
 
         try:
-            message = create_user('itsfoss')
+            message = create_user(user_name)
             return jsonify({"message": message}), 200
         except UserExistsAlready:
             return jsonify({"error": f"user {user_name} already exists"}), 409
-    
+
+@app_views.route('/<int:user_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def get_user(user_id: int) -> str:
+    """
+    /api/:user_id
+    Returns the user based on the ID
+    Args:
+        user_id (int): User's ID
+    """
+    if request.method == "GET":
+        try:
+            user = get_user_by_id(user_id)
+            return jsonify(user), 200
+        except NoResultsFound:
+            return jsonify({"error": f"User with id {user_id} doesn't exists"}), 404
+    elif request.method == "DELETE":
+        try:
+            message = delete_user_id(user_id)
+            return jsonify({"message": message}), 200
+        except NoResultsFound:
+            return jsonify({"error": f"User  with ID {user_id} not found"}), 404
+
+@app_views.route('/<string:name>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def get_user_name(name: str) -> str:
+    """
+    /api/:user_name
+    Returns the user based on the ID
+    Args:
+        user_id (int): User's ID
+    """
+    if request.method == "GET":
+        try:
+            user = get_user_by_name(name)
+            return jsonify(user), 200
+        except NoResultsFound:
+            return jsonify({"error": f"User with name {name} doesn't exists"}), 404
+    elif request.method == "DELETE":
+        try:
+            message = delete_user_name(name)
+            return jsonify({"message": message}), 200
+        except NoResultsFound:
+            return jsonify({"error": f"User {name} not found"}), 404
